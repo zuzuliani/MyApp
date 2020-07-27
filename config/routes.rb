308125813b8a36471f4eee27491d26912b997ffa
zuliani <mateus.zuliani@gmail.com>
2020-07-27 16:06:13 -0300

    V1.1
    V1 = inicial
    .1 = set roots to posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'posts#index'
+
   get 'more_info', to: 'posts#more'
 end
